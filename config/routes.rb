0c828f442046f0da6d9cca41206ee139617bb692
Blake <blakemcd@gmail.com>
2021-11-29 11:28:02 +1100

    got apis working

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
-  resources :orders
-  resources :product_orders
-  resources :stocks
-  resources :images
-  resources :products
-  resources :addresses
-  resources :stores
+ 
+  
   namespace :api do 
       namespace :v1 do
         resources :users
         resources :stores
+        resources :products
+        resources :orders
+        resources :product_orders
+        resources :addresses
+        resources :stocks
+        resources :images
         post 'login', to: 'auth#create'
       end
   end
